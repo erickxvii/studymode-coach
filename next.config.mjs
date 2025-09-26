c0e75afe706b0159e082b216578256a34a5db198
erickxvii <ebperez05@gmail.com>
2025-09-26 00:27:57 -0400

    Remove next.config.mjs to fix build

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  printf '/** @type {import("next").NextConfig} */\nconst nextConfig = {};\n\nexport default nextConfig;\n' > next.config.mjs
-
-};
-
-export default nextConfig;
-
